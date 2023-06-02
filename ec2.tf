308aa1382512cce73547db14c09adda8691d02b4
Mohammad Faheem <Fahimmd333@gmail.com>
2023-06-02 10:29:16 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+# Configure the aws Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0715c1897453cabd1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "myec2"
+  }
+}
